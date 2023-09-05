59af8430a7a4a188e4d3896f9fec83512dd20625
Programming-Beast <alexander@hulbe.de>
2023-09-05 14:22:35 +0200

    more dilara

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ const App = () => {
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
+          {/* <iframe
+            name="3-dimage"
+            width="800"
+            height="600"
+            src="https://make3d.app/render?url=https%3A%2F%2Fi.imgur.com%2FUv4RsVT.png&size=256"
+            scrolling="no"
+            frameborder="0" /> */}
         </div>
         <About />
         {/* <Experience /> */}
